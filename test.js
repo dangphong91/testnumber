f48a196ab13cce05bb8e05c79f3a2abecb26114c
dphong08 <dphongg199x@gmail.com>
2021-03-08 11:12:58 +0700

    edit

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 let diem = prompt("Nhập điểm :");
 let d = parseInt(diem);
+if ((d >= 0) && (d <= 10)) {
 (d >= 9) ? alert("XS") :
     (d >= 8) ? alert("Giỏi") :
         (d >= 7) ? alert("Khá") :
             (d >= 5) ? alert("TB") :
                 alert("Yếu");
+} else alert("Error")
